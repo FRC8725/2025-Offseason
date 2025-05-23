0647fd2bd9dd5be718dec54d3e9d4cf7ccea5ec7
ChenYueTW <eagleh2005@gmail.com>
2025-05-23 20:56:12 +0800

    Remove unnecessary lib

--- src/main/java/frc/robot/lib/helpers/IDashboardProvider.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.lib.helpers;
-
-public interface IDashboardProvider {
-    void putDashboard();
-
-    default void registerDashboard() {
-        DashboardHelper.register(this);
-    }
-}
--- src/main/java/frc/robot/lib/helpers/SubsystemProvider.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.lib.helpers;
-
-public interface SubsystemProvider {
-    default void periodic() {}
-}
